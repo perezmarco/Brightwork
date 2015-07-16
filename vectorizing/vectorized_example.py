aa0d8d82f1f4bf95059260bb584461cf810635ac
Marco Perez <mapg8790@gmail.com>
2015-07-16 11:14:27 -0700

    fixed indexing issue, now both match

--- vectorizing/vectorized_example.py
+++ vectorizing/vectorized_example.py
@@ -13,30 +13,19 @@ plt.subplot(211)
 # Make anything in the green channel less than 40 be a zero
 
 time1 = time.time()
-print image.shape, ' before vectorized changes. The mean is ', np.mean(image)
+#print image.shape, ' before vectorized changes. The mean is ', np.mean(image)
 ## Built in vectorized functions
-boolArray = image[:,:,1] < 40
 #print boolArray
-image[boolArray] = 0 #need to make sure only the greens are affected
+image[:,:,1][ image[:,:,1] < 40 ] = 0 #need to make sure only the greens are affected
 print image.shape, ' after vectorized changes. The mean is ', np.mean(image) 
-## Built in vectorized functions
-
-# ASIDE: INDEXING WITH TRUTH ARRAYS
-#############################
-# a = np.array([[1,2,3],[4,5,6]])
-# a >=3
-# a[ a>=3 ]
-#############################
-
 time2 = time.time()
-
 timevec = (time2 - time1)
 print 'time for vectorized: ', timevec
 
 plt.imshow(image)
-plt.show()
 image = plt.imread("img_0001.jpg")
 print image.shape, ' Just loaded, should match first line. The mean is ', np.mean(image)
+
 ################ Loop ##########
 
 time3 = time.time()
@@ -49,7 +38,7 @@ for i in xrange(0, image.shape[0]):
 time4 = time.time()
 
 timeloop = (time4 - time3)
-print image.shape, ' After loop, shouldn\'t change', np.mean(image[:,:,1:2])
+print image.shape, ' After loop, shouldn\'t change', np.mean(image)
 print 'time for loops: ', timeloop
 
 print 'vectorized is', (timeloop / timevec), 'times faster'
